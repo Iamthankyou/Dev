80b122ee7ac08d0b3e7f872c92f080d39f1b830c
Lê Quang Thọ <54834124+LeQuangTho@users.noreply.github.com>
2020-09-29 16:42:11 +0700

    Bat dau lam may IPOS

---
+++ (C#) LapTrinhTrucQuan/My IPOS/My IPOS/Login.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace My_IPOS
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.btThoatDangNhap = new System.Windows.Forms.Button();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.lbUserName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(138, 62);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(160, 26);
+            this.tbPassword.TabIndex = 13;
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUserName.Location = new System.Drawing.Point(138, 21);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(160, 26);
+            this.tbUserName.TabIndex = 12;
+            // 
+            // btThoatDangNhap
+            // 
+            this.btThoatDangNhap.BackColor = System.Drawing.Color.Transparent;
+            this.btThoatDangNhap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btThoatDangNhap.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btThoatDangNhap.Location = new System.Drawing.Point(172, 108);
+            this.btThoatDangNhap.Name = "btThoatDangNhap";
+            this.btThoatDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btThoatDangNhap.TabIndex = 11;
+            this.btThoatDangNhap.Text = "Thoát";
+            this.btThoatDangNhap.UseVisualStyleBackColor = false;
+            this.btThoatDangNhap.Click += new System.EventHandler(this.btThoatDangNhap_Click);
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.BackColor = System.Drawing.Color.Transparent;
+            this.btnDangNhap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDangNhap.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnDangNhap.Location = new System.Drawing.Point(53, 108);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangNhap.TabIndex = 10;
+            this.btnDangNhap.Text = "Đăng Nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = false;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword.Location = new System.Drawing.Point(12, 66);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(77, 19);
+            this.lbPassword.TabIndex = 9;
+            this.lbPassword.Text = "Password:";
+            // 
+            // lbUserName
+            // 
+            this.lbUserName.AutoSize = true;
+            this.lbUserName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUserName.Location = new System.Drawing.Point(12, 22);
+            this.lbUserName.Name = "lbUserName";
+            this.lbUserName.Size = new System.Drawing.Size(90, 19);
+            this.lbUserName.TabIndex = 8;
+            this.lbUserName.Text = "User Name:";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(331, 146);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.btThoatDangNhap);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUserName);
+            this.Name = "Login";
+            this.Text = "Đăng Nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Button btThoatDangNhap;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Label lbUserName;
+    }
+}
---
+++ (C#) LapTrinhTrucQuan/My IPOS/My IPOS/Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_IPOS
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string userName = "admin", password = "admin";
+            if (tbUserName.Text != "" && tbPassword.Text != "")
+            {
+                if (userName != tbUserName.Text && password != tbPassword.Text)
+                {
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bạn chưa nhập User hoặc Password!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btThoatDangNhap_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
